9bf91524d222ef3eef417164f7abc49377a631c7
Fred Yankowski <fcy@web.devnet.imsa.edu>
2014-03-11 15:11:58 -0500

    Report all files modified (during the latency period) on one output line. Run the command once for all reported filenames. Define and use types for Filename and Command.

--- watcher.go
+++ watcher.go
@@ -11,55 +11,44 @@ import (
 	"time"
 )
 
-type filename string
+type Filename string
+type Command string
 
-var command = flag.String("command", "/bin/ls", "command to run")
-var directory = flag.String("directory", "/var/tmp", "directory to watch for changes")
 var debug = flag.Bool("debug", false, "print debug output")
 var verbose = flag.Bool("verbose", false, "print verbose output")
 var dryrun = flag.Bool("dryrun", false, "do not execute command")
-var nostamp = flag.Bool("nostamp", false, "no datetime stamp for log output")
-
-var latency = 2
-
-func handle(filename string) {
-	if *verbose {
-		log.Printf("About to run command: %s %s", *command, filename)
-	}
-	if ! *dryrun {
-		args := append(strings.Split(*command, " "), filename)
-		out, err := exec.Command(args[0], args[1:]...).CombinedOutput()
-		if err != nil {
-			log.Printf("Command failed: %v\n%s", err, out)
-			return
-		}
-		log.Printf("Output is %s", out)
-	}
-}
 
 func main() {
+	var commandflag = flag.String("command", "", "command to run")
+	var nostamp = flag.Bool("nostamp", false, "no datetime stamp for log output")
+	var latency = flag.Duration("latency", time.Second, "seconds to wait for notifications to settle")
+
 	log.SetOutput(os.Stderr)
 	flag.Parse()
 	if *nostamp {
 		log.SetFlags(0)
 	}
-	log.Printf("Watching %v to run command '%v'", *directory, *command)
+	var directories = flag.Args()
+	var command = Command(*commandflag)
+	if *debug { log.Printf("Command is \"%v\"", command) }
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal("Error: fsnotify.NewWatcher: ", err)
 	}
 	done := make(chan bool)
-	modified := make(chan string)
+	modified := make(chan Filename)
+	toreport := make(chan Filename, 100)
 
-	// Read events from the watcher, passing on only Modify events to
-	// the 'modified' channel.
+	// Read events from the watcher and for interesting events pass
+	// the filename on to the 'modified' channel.
     go func() {
         for {
             select {
             case ev := <-watcher.Event:
-                if *debug { log.Println("event:", ev) }
+                if *debug { log.Println("from watcher.Event:", ev) }
 				if ev.IsCreate() || ev.IsModify() {
-					modified <- ev.Name
+					modified <- Filename(ev.Name)
 				}
             case err := <-watcher.Error:
                 log.Println("Error: watcher.Error:", err)
@@ -72,23 +61,78 @@ func main() {
 	go func() {
 		timer := time.NewTimer(time.Second)
 		timer.Stop()
-		var filename string
+		var filename Filename
 		for {
 			select {
 			case filename = <-modified:
-				if *debug { log.Println("modified event:", filename) }
-				timer.Reset(time.Duration(latency) * time.Second)
+				if *debug { log.Println("from modified:", filename) }
+				toreport <- filename
+				timer.Reset(time.Duration(*latency))
 			case <-timer.C:
-				fmt.Println(filename)
-				handle(filename)
+				reportall(toreport, command)
 			}
 		}
 	}()
 
-    err = watcher.Watch(*directory)
-    if err != nil {
-        log.Fatalf("Error: watcher.Watch(%s): %s", *directory, err)
-    }
+	for _, directory := range directories {
+		log.Printf("Watching %v", directory)
+		err = watcher.Watch(directory)
+		if err != nil {
+			log.Fatalf("Error: watcher.Watch(%s): %s", directory, err)
+		}
+	}
     <-done
     watcher.Close()
+}
+
+// reportall reads all filenames from the channel (without blocking),
+// removes duplicates, reports the filenames on one line of output,
+// and runs the command (if any) on the filenames.
+func reportall(toreport chan Filename, command Command) {
+	names := getall(toreport)
+	snames := make([]string, len(names))
+	for i := range names {
+		snames[i] = string(names[i])
+	}
+	fmt.Println(strings.Join(snames, "\t"))
+	if command != "" {
+		handle(names, command)
+	}
+}
+
+// getall reads all the buffered filenames from the channel (without
+// blocking) and returns an array of the unique values.
+func getall(toreport chan Filename) []Filename {
+	reported := make(map [Filename] bool)
+	names := make([]Filename, 0)
+	for {
+		select {
+		case filename := <-toreport:
+			if ! reported[filename] {
+				names = append(names, filename)
+				reported[filename] = true
+			}
+		default:
+			return names
+		}
+	}
+}
+
+// handle runs the given shell command on the array of filenames.
+func handle(filenames []Filename, command Command) {
+	args := strings.Split(string(command), " ")
+	for _, filename := range filenames {
+		args = append(args, string(filename))
+	}
+	if *verbose {
+		log.Printf("About to run command: %v", strings.Join(args, ""))
+	}
+	if ! *dryrun {
+		out, err := exec.Command(args[0], args[1:]...).CombinedOutput()
+		if err != nil {
+			log.Printf("Command failed: %v\n%s", err, out)
+			return
+		}
+		log.Printf("Output is %s", out)
+	}
 }
