356198a9f338f1da78edd30fb29d80243e2d180b
Fred Yankowski <fcy@imsa.edu>
2014-03-12 15:29:39 -0500

    Extend the example.

--- watcher_test.go
+++ watcher_test.go
@@ -3,26 +3,39 @@ package watcher
 import (
 	"log"
 	"os"
+	"regexp"
 	"time"
 )
+
+func touch(filename string) {
+	fp, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fp.Close()
+}
 
 func ExampleWatchdirs() {
 	dirs := []string{"/tmp", "/var/tmp"}
 	var opts Options
 	opts.Latency = 200 * time.Millisecond
+	opts.Exclude = regexp.MustCompile("/x[^/]*$")
 	done := make(chan bool)
 	//*Debug = true
 	go func() {
 		Watchdirs(dirs, &opts, done)
 	}()
+
 	time.Sleep(200 * time.Millisecond) // allow Watchdirs to set up
-	fp, err := os.Create("/var/tmp/foo")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fp.Close()
+	touch("/var/tmp/foo")
+	touch("/var/tmp/bar")
 	time.Sleep(time.Second)		// must be longer than the latency set above
+	touch("/var/tmp/xfoo")
+	touch("/var/tmp/blah")
+	time.Sleep(time.Second)
 	done <- true
+
 	// Output:
-	// /var/tmp/foo
+	// /var/tmp/foo	/var/tmp/bar
+	// /var/tmp/blah
 }
