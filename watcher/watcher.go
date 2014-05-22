fb573a6a6d248c5d1f3cadc504fc3b0b460daca3
Fred Yankowski <fcy@app1.devnet.imsa.edu>
2014-05-22 09:34:40 -0500

    Add option to watch subdirectories.

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -3,9 +3,11 @@
 package main
 
 import (
+	"path/filepath"
 	"flag"
 	"github.com/fredcy/watcher"
 	"log"
+	"os"
 	"regexp"
 	"time"
 )
@@ -15,6 +17,7 @@ func main() {
 	var nostamp = flag.Bool("nostamp", false, "no datetime stamp for log output")
 	var latency = flag.Duration("latency", time.Second, "seconds to wait for notifications to settle")
 	var excludeflag = flag.String("exclude", "", "pattern of files to ignore")
+	var subdirflag = flag.Bool("subdirs", false, "watch subdirectories too")
 
 	flag.Parse()
 	if *nostamp {
@@ -27,6 +30,33 @@ func main() {
 	var exclude *regexp.Regexp
 	if *excludeflag != "" {
 		exclude = regexp.MustCompile(*excludeflag)
+	}
+
+	if *subdirflag {
+		subdirs := make([]string, 0)
+		baddirs := make(map[string]bool)
+		walkfn := func(path string, info os.FileInfo, err error) error {
+			//log.Printf("walkfn(%v, %v, %v)", path, info, err)
+			if err != nil {
+				log.Printf("warning: %v", err)
+				if info.IsDir() {
+					baddirs[path] = true
+				}
+				return nil
+			}
+			if info.IsDir() {
+				subdirs = append(subdirs, path)
+			}
+			return nil
+		}
+		for _, directory := range(directories) {
+			filepath.Walk(directory, walkfn)
+		}
+		for _, dir := range(subdirs) {
+			if ! baddirs[dir] {
+				directories = append(directories, dir)
+			}
+		}
 	}
 
 	done := make(chan bool)
